2e9530f07ff7697396195819e4edc65e72fa48f4
Ivo Tasevski <ivo_t@live.com>
2021-02-19 01:28:56 +0100

    Phase 1 - HTML (commit 1)

---
+++ Code/Recruitment/src/main/java/mk/ukim/finki/recruitment/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package mk.ukim.finki.recruitment.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/", "/home"})
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage(Model model) {
+        model.addAttribute("bodyContent", "home");
+        return "master-template";
+    }
+
+}
